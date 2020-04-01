581df2aede9be1b3892e4033cc2abb2e25ead071
whdkwhdk90 <62650790+whdkwhdk90@users.noreply.github.com>
2020-04-01 23:27:26 +0900

    자바6일차

--- 홈워크/HW1_LogicHomework/src/com/home/controller/Function.java
+++
@@ -1,6 +0,0 @@
-package com.home.controller;
-
-public class Function {
-
-		public 
-}
--- 홈워크/HW1_LogicHomework/src/com/home/main/TestMain.java
+++
@@ -1,9 +0,0 @@
-package com.home.main;
-
-import com.home.view.Menu;
-
-public class TestMain {
-	public static void main(String[] args) {
-		new Menu().displayMenu();   //과제 1 해결
-	}
-}
